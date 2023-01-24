2682b8052bb8b987a5e887d4a733a745b917c54f
soo <ssuh0o0@daum.net>
2023-01-24 23:20:37 +0900

    2022/01/24 후.. 1 더하는걸 이해 못해서 다른 방식으로 이해한 ...근데 말하면 서 이해해버림..

--- 누적합/나머지 합.py
+++ 누적합/나머지 합.py
@@ -1,16 +1,17 @@
 # O(n^2)의 시간을 돌아서 시간초과 코드
-
 import sys
 n, m = map(int,sys.stdin.readline().split())
 arr = list(map(int,sys.stdin.readline().split()))
-sums = [0] * n
-sums[0] = arr[0]
-for idx in range(1,n):
-    sums[idx] = sums[idx-1] + arr[idx]
 
-cnt = 0
+aSum = 0
+sums = [0] * m
 for i in range(n):
-    for j in range(i,n):
-        if (sums[j] - sums[i]) % m :
-            cnt += 1
-print(cnt)
+    aSum += arr[i]
+    sums[aSum%m] += 1
+
+answer = sums[0]
+for a in range(n):
+    answer += a * (a-1) //2
+print(answer)
+
+
