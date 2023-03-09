b37182ad951b4258cd7a20ae353d0ff0f6ba7cfe
soo <ssuh0o0@daum.net>
2023-03-09 18:43:00 +0900

    2023/03/09 이분탐색으로 이런문제도 풀 수 있네..

---
+++ 이분탐색/K번째 수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+k = int(input())
+
+start = 1
+end = k
+
+answer = 0
+while start <= end:
+    cnt = 0
+    mid = ( start + end )//2
+    
+    for i in range(1, n+1):
+        cnt +=  min(mid//i, n)
+    
+    if cnt < k :
+        start = mid + 1
+    else:
+        end = mid - 1
+        answer = mid
+print(answer)
