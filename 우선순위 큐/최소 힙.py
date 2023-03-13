2aa8d51df5873e7e91af6ead973312fadb07c7fc
soo <ssuh0o0@daum.net>
2023-03-13 19:09:19 +0900

    2023/03/13 최소힙은 최대힙보다 쉬운듯?

---
+++ 우선순위 큐/최소 힙.py
@@ -0,0 +1,15 @@
+import sys
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
+n = int(input())
+heap = []
+for _ in range(n):
+    num = int(input())
+    if not num:
+        if not len(heap):
+            print(0)
+        else:
+            print(heappop(heap))  
+    else:
+        heappush(heap, num)
